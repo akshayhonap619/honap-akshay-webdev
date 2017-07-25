dc37af2ff912e758aaf480b16860806c4b1cba2a
akshayhonap619 <akshayhonap619@gmail.com>
2017-07-25 19:46:32 -0400

    Update login.controller.client.js

--- public/assignment/user/controllers/login.controller.client.js
+++ public/assignment/user/controllers/login.controller.client.js
@@ -1,3 +1,38 @@
 /**
  * Created by Akshay on 7/21/2017.
  */
+(function () {
+    var app = angular.module("WebApp");
+
+
+
+    app.controller("loginController", loginController);
+
+    function loginController($location, userService) {
+
+        var model = this;
+        model.login = login;
+        model.register = register;
+        model.welcomeUser = undefined;
+            function login(user) {
+
+            var myuser = userService.findUserByUserName(user.username, user.password);
+
+            if (myuser != null) {
+                $location.url("user" + "/" + myuser._id);
+            }
+            else {
+                model.welcomeUser = "User not found ";
+            }
+        }
+
+             function register() {
+                $location.url("register");
+            }
+
+        }
+
+
+
+
+})();
