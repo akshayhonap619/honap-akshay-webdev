39c31c301947464f9366e50ec25634535fae4a37
akshayhonap619 <akshayhonap619@gmail.com>
2017-07-25 19:47:11 -0400

    Update register.controller.client.js

--- public/assignment/user/controllers/register.controller.client.js
+++ public/assignment/user/controllers/register.controller.client.js
@@ -1,3 +1,50 @@
 /**
- * Created by Akshay on 7/23/2017.
+ * Created by Akshay on 7/21/2017.
  */
+(function () {
+    var app = angular.module("WebApp");
+
+
+
+    app.controller("registerController", registerController);
+
+    function registerController($location, userService) {
+
+        var model = this;
+        model.register = function (username, password, password1) {
+
+            if(username === null || username === '' || username === undefined ) {
+                model.errorMessage = "UserName Cannot be null";
+                return;
+            }
+
+            if(!(password === password1)){
+                model.errorMessage = "Passwords must Match";
+                return;
+            }
+
+
+            var myuser = userService.findUserByUserNameonly(username);
+
+            if(myuser!=null)
+            {
+                model.errorMessage = "User already exists";
+                return;
+            }
+            else{
+
+                var id = userService.addUser(username, password);
+                $location.url("/user/"+id);
+            }
+
+
+
+
+        }
+
+    }
+
+
+
+
+})();
