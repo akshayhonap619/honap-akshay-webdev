de679a011b3a677bfa9d49d0423e43510f404813
akshayhonap619 <akshayhonap619@gmail.com>
2017-07-25 19:46:53 -0400

    Update profile.controller.client.js

--- public/assignment/user/controllers/profile.controller.client.js
+++ public/assignment/user/controllers/profile.controller.client.js
@@ -1,3 +1,38 @@
 /**
  * Created by Akshay on 7/21/2017.
  */
+(function()
+{
+     angular.module("WebApp")
+         .controller("profileController", profileController);
+
+
+
+
+    function profileController($routeParams, userService,$location) {
+
+        var model = this;
+        model.userId = $routeParams.userId;
+        model.updateUser = updateUser;
+        model.deleteUser = deleteUser;
+        init();
+
+        function init() {
+
+            model.user = userService.findUserById(model.userId);
+            console.log(model.user.username);
+        }
+
+        function updateUser() {
+            userService.updateUser(model.userId, model.user.firstName, model.user.lastName, model.user.username, model.user.email);
+            $location.url("/user/"+model.userId);
+        }
+
+        function deleteUser() {
+            userService.deleteUser(model.userId);
+            $location.url("/login");
+        }
+
+    }
+
+})();
