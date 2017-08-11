ac040a30d37bb22ad12e15eb40025c65e3b3c0e1
akshayhonap619 <honap.a@husky.neu.edu>
2017-08-10 23:59:40 -0400

    project test

--- public/project/js/testController.js
+++ public/project/js/testController.js
@@ -7,7 +7,7 @@
     angular.module("JobApp", [])
         .controller("searchController",searchController);
 
-    function searchController($http ,$location) {
+    function searchController($http ,$location, $sce) {
 
         var model = this;
 
@@ -15,15 +15,24 @@
 
 
         function searchJob(text) {
-            var url = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+text;
-            console.log(text);
-            $http.get(url)
+          //  var url = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+text;
+
+          var url =   "https://authenticjobs.com/api/?api_key=e83067aa5a22e95e99214b5f44ba9372&method=aj.jobs.search&format=json";
+          //url = url.substring(0,url.length-2);
+            console.log(url);
+            $sce.trustAsResourceUrl(url);
+            $http.jsonp(url)
                .then(function (response) {
-                   model.results = response.data.resultItemList;
+                   console.log(response);
+
+                   // model.results = response.data.resultItemList;
                    //return response.data.resultItemList;
+                   console.log(model.results);
+                },function (error) {
+                   console.log(error);
                });
 
-           //console.log(model.results);
+
 
         }
         
